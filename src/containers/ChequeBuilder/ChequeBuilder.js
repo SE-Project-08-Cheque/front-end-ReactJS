6e7fde5adc6f46bc2c27d73d13ae7c9c58527964
Dakshitha Suriyaaratchie <dakshitha.18@cse.mrt.ac.lk>
2021-01-18 11:15:06 +0530

    Save before eject

---
+++ src/containers/ChequeBuilder/ChequeBuilder.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxi';
+class ChequeBuilder extends Component{
+    render(){
+        return(
+            <Aux>
+                <div>ChequeBulider</div>
+            </Aux>
+        );
+    }
+}
+export default ChequeBuilder;
